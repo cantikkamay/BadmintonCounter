7c4d8c29291836dc4aa90fa3873945a1c496e6c9
cantikkamay <cantikkamayaristianti@gmail.com>
2025-03-05 12:56:50 +0700

    penambahan MainActivity.kt

--- app/src/main/java/com/cantikka/badmintoncounter/MainActivity.kt
+++ app/src/main/java/com/cantikka/badmintoncounter/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var skorA: TextView
     private lateinit var skorB: TextView
 
